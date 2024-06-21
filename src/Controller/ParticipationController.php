626897d7ab291b2822e90f5bd1dea43f1aceae4e
Titouan <titouanmagnin2@gmail.com>
2024-06-21 18:19:52 +0200

    css

--- src/Controller/ParticipationController.php
+++ src/Controller/ParticipationController.php
@@ -4,7 +4,7 @@ namespace App\Controller;
 
 use Exception;
 use App\Entity\Evenement;
-use Symfony\Component\Mime\Email;
+// use Symfony\Component\Mime\Email;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Mailer\MailerInterface;
@@ -24,24 +24,24 @@ class ParticipationController extends AbstractController
     }
 
     #[Route('/participation/add/{id}', name: 'app_participation_add')]
-    public function add(EntityManagerInterface $entityManager,Evenement $event, MailerInterface $mailer): Response
+    public function add(EntityManagerInterface $entityManager,Evenement $event): Response
     {
         $user=$this->getUser();
         if ($user==null)
             return throw new Exception("User not connected");
         // echo $user->getEmail();
-        $email = (new Email())
-            ->from('bot.love.test@gmail.com')
-            // ->to($user->getEmail())
-            ->to('bot.love.test@gmail.com')
-            // ->priority(Email::PRIORITY_HIGH)
-            ->subject('Time for Symfony Mailer!')
-            ->text('Sending emails is fun again!')
-            ->html('<p>See Twig integration for better HTML integration!</p>');
+        // $email = (new Email())
+        //     ->from('bot.love.test@gmail.com')
+        //     // ->to($user->getEmail())
+        //     ->to('bot.love.test@gmail.com')
+        //     // ->priority(Email::PRIORITY_HIGH)
+        //     ->subject('Time for Symfony Mailer!')
+        //     ->text('Sending emails is fun again!')
+        //     ->html('<p>See Twig integration for better HTML integration!</p>');
 
         $user->addEvenement($event);
         $entityManager->flush();
-        $toto = $mailer->send($email);
+        // $toto = $mailer->send($email);
         // dd($toto);
         
         return $this->redirectToRoute('app_participation_user', [], Response::HTTP_SEE_OTHER);  
--- src/Form/Evenement1Type.php
+++ src/Form/Evenement1Type.php
@@ -4,7 +4,9 @@ namespace App\Form;
 
 use App\Entity\Evenement;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\DateTime;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class Evenement1Type extends AbstractType
@@ -14,7 +16,7 @@ class Evenement1Type extends AbstractType
         $builder
             ->add('titre')
             ->add('description')
-            ->add('date', null, [
+            ->add('date', DateType::class, [
                 'widget' => 'single_text',
             ])
             ->add('nbMax')
